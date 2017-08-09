6c95f96023ca6b3ad918c5256813b37d9546a1d9
Deslyxia <cmcmahon1978@gmail.com>
2017-08-08 23:28:45 -0700

    Initial Commit

---
+++ src/main/java/com/cmcmahon/json/data/JsonObject.java
@@ -0,0 +1,7 @@
+package com.cmcmahon.json.data;
+
+/**
+ * Created by Deslyxia on 8/8/17.
+ */
+public class JsonObject {
+}
---
+++ src/main/java/com/cmcmahon/json/parser/JsonParser.java
@@ -0,0 +1,8 @@
+package com.cmcmahon.json.parser;
+
+/**
+ * Created by Deslyxia on 8/8/17.
+ */
+public class JsonParser {
+    
+}
